6a177c6416e030f9c9e27fcdbfa9e9cf82dcfa3d
Irfan <801.mirfan@gmail.com>
2025-11-19 11:26:28 +0530

    Integrate Redis caching into views.py for optimized response handling

--- blog/views.py
+++ blog/views.py
@@ -13,16 +13,33 @@ from django.views.generic import (
                         DeleteView,
                         )
 
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
-
+ 
 
 class PostListView(ListView):
     """ Home Page View - List all posts """
+    print("Getting the List from DB")
     model = Post
     template_name = 'blog/home.html'  # Template to render 'blog/post_list.html
     context_object_name = 'posts'     # Custom template used default is 'object_list'
     paginate_by = 4
     ordering = ['-date_posted']       # Newest posts first ('-' indicates descending order by date)
+
+    def get_queryset(self):
+        cache_key = ('post_list_view')
+        posts = cache.get(cache_key)
+
+        if posts is not None:
+            print('Returning from Cache')
+            return posts
+        
+        print('Returning from DB')
+        posts = list(Post.objects.all().order_by('-date_posted'))
+        cache.set(cache_key, posts, 60 * 60)
+        return posts
 
 
 class SameUserPostListView(ListView):
@@ -31,14 +48,31 @@ class SameUserPostListView(ListView):
     template_name = 'blog/same_user_posts.html' 
     context_object_name = 'SamePosts' 
     paginate_by = 4
-      
+    
     def get_queryset(self):
-        # Fetch user object using username from URL 
+        username = self.kwargs.get('username')
+        cache_key = f"user_posts_{username}"  # ensures each user's posts are cached separately ex: 
+        
+        # Try to get from cache
+        posts = cache.get(cache_key)
+        
+        if posts is not None:
+            print(f"Getting posts for {username} from CACHE")
+            return posts
+        
+        # Cache empty - fetch from database
+        print(f"Getting posts for {username} from DB")
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_posted') 
+        posts = list(Post.objects.filter(author=user).order_by('-date_posted'))
 
+        # Cache for 30 minutes
+        cache.set(cache_key, posts, timeout=60 * 30)
+        return posts
+        
 
-
+""" Manual caching here.............Because you'd only be replacing the database lookup but still rendering the template on every request. 
+Page caching does both — it stores and serves the final HTML. That saves more time and more server resources. """
+@method_decorator(cache_page(60 * 60, key_prefix='post_detail'), name='dispatch')
 class PostDetailView(DetailView):
     """ Post Detail View - Shows a single post"""
     model = Post
@@ -61,7 +95,7 @@ class PostCreateView(AuthorRequiredMixin, LoginRequiredMixin, CreateView):
 
 
 class PostUpdateView(AuthorRequiredMixin, LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    """Allows logged in authors(users) to update their own posts. """
+    """Allows logged in users(authors) to update their own posts. """
     model = Post
     fields = ['title', 'content_nature', 'content']  
 
